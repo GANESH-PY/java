fa3d5187e312f137bb7b59acaf90a3bc30693a8a
akganesh <65601890+GANESH-PY@users.noreply.github.com>
2024-06-10 08:23:18 +0530

    a value changed

--- demo.java
+++ demo.java
@@ -1,7 +1,7 @@
 public class demo {
     public static void main(String args[]){
 
-        int a = 10;
+        int a = 15;
         System.out.println("New Line"+a);
         System.out.println("Hello World!");
     }
